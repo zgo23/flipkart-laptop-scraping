987cb8772bb9080b313611ffba4497d594eb8006
Zhigang Gong (Matthew) <zgo23@uclive.ac.nz>
2020-09-08 21:45:41 +1200

    Got the search result of "laptop"

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # flipkart-laptop-scraping
+
 Using Python and related tools to extract laptop products information, e.g name, price and rating, from Flipkart.com.
+
+Prepare the developement environment
+
+Referecnces
+Selenium, https://www.selenium.dev/
+Selenium with Python documentation, https://selenium-python.readthedocs.io/
+BeautifulSoup Documentation, https://www.crummy.com/software/BeautifulSoup/bs4/doc/#
+
+1. Set up the Selenium
+   1.1 Install Python bindings for Selenium
+   pip install selenium
+
+1.2 Download and configure the ChromeDriver
+On Mac, copy the ChromeDriver to the /usr/local/bin
+
+3. Install BeautifulSoup
+   pip install beautifulsoup4
+
+4. Install Pandas
+   pip install pandas
---
+++ web-s.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+# from BeautifulSoup import BeautifulSoup
+# import pandas as pd
+
+FIND_ELEMENT_TIMEOUT = 30
+
+websiteUrl = 'http://www.flipkart.com'
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get(websiteUrl)
+
+try:
+    login_popup_close_button = WebDriverWait(driver, FIND_ELEMENT_TIMEOUT).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '._2AkmmA._29YdH8')))
+except TimeoutException:
+    pass
+else:
+    login_popup_close_button.click()
+    print('The Login pop-up was closed.')
+
+try:
+    search_input = WebDriverWait(driver, FIND_ELEMENT_TIMEOUT).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.LM6RPg')))
+except TimeoutException:
+    driver.quit()
+else:
+    search_input.clear()
+    search_input.send_keys('laptop')
+    print('The search term "laptop" was set.')
+
+searchButton = driver.find_element_by_css_selector(".vh79eN")
+searchButton.click();
+print('Start to search laptop products.')
+
+try:
+    search_result_summary_element = WebDriverWait(driver, FIND_ELEMENT_TIMEOUT).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '._2yAnYN')))
+except TimeoutException:
+    driver.quit()
+else:
+    print(search_result_summary_element.get_attribute('innerText'))
+
+driver.quit()
+
+
